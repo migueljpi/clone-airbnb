20b70c1540ee448329f4daa1e235caef05eb1f88
MauroSilva17 <m.henriquesilva18@gmail.com>
2025-03-01 13:16:42 +0100

    fix: update models

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -3,13 +3,12 @@ class Tour < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_many :users, through: :bookings
   belongs_to :user
-  has_many_attached :photos
+  has_one_attached :photo
   validates :name, presence: true
   validates :location, presence: true
   validates :description, presence: true
   validates :duration_in_hours, presence: true, numericality: { only_integer: true }
   validates :price, presence: true, numericality: { only_integer: true }
   # validates :starting_time, presence: true
-  validates :category, inclusion: { in: CATEGORIES, message: "%{value} is not a valid type" }
-
+  validates :category, inclusion: { in: CATEGORIES, message: "%<value>s is not a valid type" }
 end

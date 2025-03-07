93fb8a34b363164b213fc82742c89e72fcfc4626
Miguel Ines <miguel.joao.ines@gmail.com>
2025-03-07 14:07:17 +0000

    fix: add lots of small scale fixes and tweaks

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,5 @@
 class Booking < ApplicationRecord
-  STATUS = ["Pending", "Confirmed", "Cancelled", "Completed", "No Show", "Refunded", "Denied"]
+  STATUS = ["Pending", "Confirmed", "Cancelled", "Completed", "No Show", "Refunded", "Rejected"]
 
   belongs_to :user
   belongs_to :tour

3c5eaa9e9f42fb1109d84e48c9f44d27828604f1
MauroSilva17 <m.henriquesilva18@gmail.com>
2025-03-11 23:17:05 +0100

    feat: add map modal

---
+++ app/javascript/controllers/map_modal_controller.js
@@ -0,0 +1,14 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="map-modal"
+export default class extends Controller {
+  static targets = ["mapModal"]
+
+  open() {
+    this.mapModalTarget.classList.remove("d-none");
+  }
+
+  close() {
+    this.mapModalTarget.classList.add("d-none");
+  }
+}
